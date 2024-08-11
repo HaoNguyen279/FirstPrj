15c8f24ea5e3148f798812a56fe9fd24f0fb8c0e
HAONGUYEN <minhhao2792005@gmail.com>
2024-08-11 14:40:07 +0700

    Nop report

--- src/Welcome.java
+++ src/Welcome.java
@@ -3,5 +3,9 @@ public class Welcome {
         System.out.println("Hello Nguyen Minh Hao");
         System.out.println("Welcome to KTPM19B");
         System.out.println("Change something");
+        void changeSomethingAgain(){
+            System.err.println("trroll");
+        }
     }
+
 }
